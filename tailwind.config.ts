db0eba97fa569bfd65f7f682391c5137fbc3133b
joassalvior <joassalvior@gmail.com>
2024-06-19 15:38:00 -0300

    fix: change some configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        gray: {
+          600: '#8A94A4',
+        }
+      }
     },
   },
   plugins: [],
