09a22ac67863bd16366d2afedd0458062f75d445
joassalvior <joassalvior@gmail.com>
2024-06-20 10:00:36 -0300

    fix: responsive accordions

--- src/components/NavItems/index.tsx
+++ src/components/NavItems/index.tsx
@@ -9,24 +9,24 @@ interface NavItemsProps {
 
 export function NavItems({ sections, id, bg }: NavItemsProps) {
   return (
-    <div className="flex justify-between ">
+    <div className="flex justify-between max-w-96">
       {sections.map((section) => (
         <div
           key={section.id}
-          className={`min-w-40 flex flex-col items-center text-center p-4 gap-4 ${section.id === id ? 'border-b-4 border-gray-800' : ''}`}
+          className={`flex flex-col items-center text-center p-4 gap-4 ${section.id === id ? 'border-b-4 border-gray-800' : ''}`}
         >
           <div className={`p-1 bg-white rounded-full  ${section.id === id ? 'border-2 border-gray-800' : ''}`}>
             <Image
-              className={`rounded-full h-28 w-28 object-cover  ${section.id === id ? 'border border-white' : ''}`}
-              width={2000}
-              height={400}
+              className={`max-w-none rounded-full h-28 w-28 max-sm:h-20 max-sm:w-20 max-lg:w-24 max-lg:h-24 object-cover ${section.id === id ? 'border border-white' : ''}`}
+              width={1000}
+              height={1000}
               src={section?.images && section?.images[0]?.image || ''}
               alt={section.name}
               style={{ borderColor: bg }}
             />
           </div>
 
-          <h2 className='text-xl font-bold'>{section.name}</h2>
+          <h2 className='text-xl max-sm:text-lg font-bold'>{section.name}</h2>
         </div>
       ))}
     </div>
